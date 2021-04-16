4c9236ce2b13a00e32dfe42afa332025d63401a4
Thomas Sankara <thomassankara74@gmail.com>
2021-04-16 22:13:08 +0300

    data arrays

---
+++ data/homepage.js
@@ -0,0 +1,34 @@
+export const homepage = [
+  {
+    id: 1,
+    title: 'My Profile',
+  },
+  {
+    id: 2,
+    title: 'Community',
+  },
+  {
+    id: 3,
+    title: 'Schedule',
+  },
+  {
+    id: 4,
+    title: 'Workouts',
+  },
+  {
+    id: 5,
+    title: 'Nutrition',
+  },
+  {
+    id: 6,
+    title: 'Activity',
+  },
+  {
+    id: 7,
+    title: 'Calendar',
+  },
+  {
+    id: 8,
+    title: 'My Progress',
+  },
+];
